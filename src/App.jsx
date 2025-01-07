243a739193eee224b545ab84ffc364326ca2f276
Shubham patil <shubham.patil@sincrodigital.com>
2025-01-07 11:59:17 +0530

    setting up a store

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Body from './components/Body'
 import Head from './components/Head'
+import { Provider } from 'react-redux'
+import store from './utils/Store'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Head />
-      <Body />
-    </>
+    <Provider store={store} >
+      <div>
+        <Head />
+        <Body />
+      </div>
+    </Provider>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,31 @@
+import { useSelector } from "react-redux"
 
 const Sidebar = () => {
+  const menuState = useSelector((state) => state.app.isMenuOpen);
+  if(!menuState) return null;
+
   return (
-    <div>Sidebar</div>
+    <div className="shadow-lg mr-2 w-1/5">
+      <ul>
+        <li>Home</li>
+        <li>Short</li>
+        <li>Subscriptions</li>
+      </ul>
+      <h1 className="font-bold mt-2 border-t-2">You</h1>
+      <ul>
+        <li>History</li>
+        <li>Playlist</li>
+        <li>Your Videos</li>
+        <li>Watch Later</li>
+      </ul>
+      <h1 className="font-bold mt-2 border-t-2">Subscriptions</h1>
+      <ul>
+        <li>History</li>
+        <li>Tech</li>
+        <li>Music</li>
+        <li>Games</li>
+      </ul>
+    </div>
   )
 }
 
